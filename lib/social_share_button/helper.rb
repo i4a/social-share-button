7bea05099060362ef9ffcea800b9f0e474a58c9e
edgarbarrero <edgar@ideas4all.com>
2020-03-02 15:24:50 +0100

    accesibility: aria label include in social link

--- lib/social_share_button/helper.rb
+++ lib/social_share_button/helper.rb
@@ -12,12 +12,13 @@ module SocialShareButton
       opts[:allow_sites].each do |name|
         extra_data = opts.select { |k, _| k.to_s.start_with?('data') } if name.eql?('tumblr')
         special_data = opts.select { |k, _| k.to_s.start_with?('data-' + name) }
-        
+
         special_data["data-wechat-footer"] = t "social_share_button.wechat_footer" if name == "wechat"
 
         link_title = t "social_share_button.share_to", :name => t("social_share_button.#{name.downcase}")
         html << link_to("", "#", { :rel => ["nofollow", rel],
                                    "data-site" => name,
+                                   :"aria-label" => name.downcase,
                                    :class => "ssb-icon ssb-#{name}",
                                    :onclick => "return SocialShareButton.share(this);",
                                    :title => h(link_title) }.merge(extra_data).merge(special_data))
